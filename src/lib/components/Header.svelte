3832ede9c4f08ec40cc7f263581f50f3ae288d25
voxal <aidenhohoho@gmail.com>
2023-05-03 22:04:57 -0700

    added layout

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,17 @@
 <script>
+  export let image = "/cyberpunk.gif";
 </script>
 
-<header class="flex flex-col h-[50vh] bg-[linear-gradient(to_top,#12101C_0%,_transparent_58.75%),url('/cyberpunk.gif')] bg-center bg-cover mb-8">
+<header
+  style:background-image="linear-gradient(to top, #12101C 0%, transparent 58.75%), url('{image}')"
+  class="flex flex-col h-[50vh] bg-center bg-cover mb-8"
+>
   <nav class="flex xm-4"><img alt="CyberClass Logo" /></nav>
   <div class="flex-1 flex justify-center items-center text-center">
-    <h1 class="text-5xl sm:text-6xl md:text-7l lg:text-8xl text-limey text-glow font-bold"><slot /></h1>
+    <h1
+      class="text-5xl sm:text-6xl md:text-7l lg:text-8xl text-limey text-glow font-bold"
+    >
+      <slot />
+    </h1>
   </div>
 </header>
