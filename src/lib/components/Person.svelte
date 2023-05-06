13cd63251b671989e81e6047904f2515ec4f6565
user <70670632+stuxf@users.noreply.github.com>
2023-05-05 18:22:35 -0700

    fix some sizing issues

--- src/lib/components/Person.svelte
+++ src/lib/components/Person.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="mt-16">
-  <img alt={altText} src={imageUrl} width=256 height=384 class="m-auto mb-4"/>
+  <img alt={altText} src={imageUrl} class="w-64 h-96 m-auto mb-4 object-contain"/>
   <h2 class="text-4xl font-bold"><slot name="name" /></h2>
   <h3 class="text-xl mb-4"><slot name="role" /></h3>
   <p><slot name="bio" /></p>
